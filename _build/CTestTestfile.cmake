e5fa9333e36c27e25aeb1287b9061b8c0998ca80
PNuikin <p.nuikin@gmail.com>
2023-05-09 21:15:56 +0300

    added tests 1

---
+++ _build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/pavel/PNuikin/workspace/projects/lab05
+# Build directory: /home/pavel/PNuikin/workspace/projects/lab05/_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/pavel/PNuikin/workspace/projects/lab05/_build/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/pavel/PNuikin/workspace/projects/lab05/CMakeLists.txt;45;add_test;/home/pavel/PNuikin/workspace/projects/lab05/CMakeLists.txt;0;")
+subdirs("third-party/gtest")
